53529cd725b74622263326deabfe8e9a0555cc05
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-11-26 13:35:57 -0600

    add unit, chapter, and quiz services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
 import { CourseService } from './course/course.service';
+import { UnitModule } from './unit/unit.module';
 
 @Module({
-  imports: [CourseModule],
+  imports: [CourseModule, UnitModule],
   controllers: [],
   providers: [AppService, CourseService],
 })
--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
+import { UnitService } from './unit.service';
+import { ChapterService } from './chapter.service';
+import { QuizService } from './quiz.service';
 
 @Module({
-  providers: [CourseService],
+  providers: [CourseService, UnitService, ChapterService, QuizService],
   controllers: [CourseController],
 })
 export class CourseModule {}
