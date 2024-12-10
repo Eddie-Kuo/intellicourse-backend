33d0c59aa41cf674ddc236eef37a884fdc7d468a
Eddie Kuo <eddiekuo97@gmail.com>
2024-12-10 14:23:51 -0600

    Fetch youtube vid data (#4)
    * fetch youtube id and video transcript with search query
    
    * clearnup
    
    * refactor initializing innertube instance
    
    * fix return value for getYoutubeVideoId function

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CourseOutput, CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
+import { YoutubeService } from './youtube.service';
 
 @Controller('course')
 export class CourseController {
-  constructor(private courseService: CourseService) {}
+  constructor(
+    private courseService: CourseService,
+    private youtubeService: YoutubeService,
+  ) {}
 
   @Get()
   sayHello() {
@@ -21,5 +25,16 @@ export class CourseController {
     @Body() createCourseDto: CreateCourseDto,
   ): Promise<CourseOutput> {
     return this.courseService.createCourse(createCourseDto);
+  }
+
+  // for testing youtube provider
+  @Post('/youtube')
+  getYoutubeVideoTranscript(@Body() query: string) {
+    return this.youtubeService.getYoutubeVideoTranscript('WB6eJmaqxGw');
+  }
+
+  @Post('/youtube/video')
+  getYoutubeVideoId(@Body() query: string) {
+    return this.youtubeService.getYoutubeVideoId('how to train a new puppy');
   }
 }
---
+++ src/course/youtube.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import Innertube from 'youtubei.js';
+
+@Injectable()
+export class YoutubeService {
+  private youtube: Innertube;
+
+  constructor() {
+    this.initYoutube().then((youtube) => {
+      this.youtube = youtube;
+    });
+  }
+
+  private async initYoutube() {
+    return await Innertube.create({
+      retrieve_player: false,
+    });
+  }
+
+  async getYoutubeVideoId(searchQuery: string): Promise<string> {
+    // hello world => hello+world
+    searchQuery = encodeURIComponent(searchQuery);
+
+    const { data } = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API}&q=${searchQuery}&videoDuration=medium&videoEmbeddable=true&type=video&maxResults=5`,
+    );
+
+    if (!data.items.length) {
+      console.log('No Youtube Videos returned from that search query');
+      return;
+    }
+
+    // return the first video
+    return data.items[0].id.videoId;
+  }
+
+  async getYoutubeVideoTranscript(videoId: string): Promise<string> {
+    try {
+      const videoInfo = await this.youtube.getInfo(videoId); // get video info by video Id
+      const transcriptData = await videoInfo.getTranscript();
+
+      if (!transcriptData?.transcript?.content?.body?.initial_segments) {
+        throw new Error('No transcript data available');
+      }
+
+      const formattedTranscript =
+        transcriptData.transcript.content?.body?.initial_segments
+          .map((segment) => segment.snippet.text)
+          .join(' ')
+          .replaceAll('\n', '');
+
+      return formattedTranscript;
+    } catch (error) {
+      console.log('Error: Error when transcribing Youtube Video', error);
+      return '';
+    }
+  }
+}
