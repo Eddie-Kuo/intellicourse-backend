6e566724dbe5ee3579f23848ef3b5f0af4519006
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-12-11 15:40:25 -0600

    refactor error handling

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -31,16 +31,12 @@ export class CourseService {
     const { topic } = createCourseDto;
 
     const generatedCourse: CourseOutput =
-      await this.openAiService.generateCourse(
-        `You are an experienced instructor on the topic of ${topic}, coming up with relevant unit titles, detailed chapters, and finding relevant youtube videos for each of the chapters.`,
-        `It is your job to create a detailed course roadmap about ${topic}. Create units for all the major topics about ${topic}. Then, for each unit, create a list of chapters breaking down the unit into more specific subtopics for the user to follow. Then for each chapter, provide a detailed youtube search query that can be used to find an informative educational video for each chapter. Each query should give an educational informative course in youtube.`,
-        {
-          title: 'Title of the course',
-          units: 'Title of the units',
-          chapters:
-            'An array of chapters covering the important topics in the unit. Each chapter with a relevant youtube_search_query and a chapter_title key in the JSON object. Be very specific on the material covered in each chapter.',
-        },
-      );
+      await this.openAiService.generateCourse(topic, {
+        title: 'Title of the course',
+        units: 'Title of the units',
+        chapters:
+          'An array of chapters covering the important topics in the unit. Each chapter with a relevant youtube_search_query and a chapter_title key in the JSON object. Be very specific on the material covered in each chapter.',
+      });
 
     await Promise.all(
       generatedCourse.units.map(async (unit) => {
@@ -62,7 +58,7 @@ export class CourseService {
             }
 
             const summary =
-              await this.openAiService.summarizeTranscript(transcript);
+              await this.openAiService.summarizeVideoTranscript(transcript);
 
             const questions: Question =
               await this.openAiService.generateQuestionsFromSummary({
