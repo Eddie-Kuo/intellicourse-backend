28e703e6aa5078fdc10497d5c13631a17984f66e
Eddie-Kuo <Eddiekuo97@gmail.com>
2025-01-10 22:22:08 -0600

    add cors

--- src/course/dto/generate-course.dto.ts
+++ src/course/dto/generate-course.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GenerateCourseDto {
   @IsNotEmpty({
     message: 'Field cannot be empty. Enter a topic to learn more about',
   })
+  @IsString()
   topic: string;
 }
