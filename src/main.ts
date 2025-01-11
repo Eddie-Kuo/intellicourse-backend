c7fd15fa08ee4e1b3978c8895796705b73041900
Eddie Kuo <eddiekuo97@gmail.com>
2025-01-10 22:24:20 -0600

    add cors (#16)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
