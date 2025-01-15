1f3b880d4bd7745de6e8ad412b3abac89c5b9c76
Eddie-Kuo <Eddiekuo97@gmail.com>
2025-01-14 18:42:08 -0600

    add pino logger configurations to course service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { CourseService } from './course/course.service';
 import { OpenAiService } from './course/openai.service';
 import { YoutubeService } from './course/youtube.service';
 import { PrismaService } from './course/prisma.service';
+import { LoggerModule } from 'nestjs-pino';
+import { colorizerFactory } from 'pino-pretty';
 
 @Module({
   imports: [
@@ -13,6 +15,28 @@ import { PrismaService } from './course/prisma.service';
       envFilePath: '.env',
     }),
     CourseModule,
+    process.env.APP_ENV == 'development'
+      ? LoggerModule.forRoot({
+          pinoHttp: {
+            transport: {
+              // formats logs to be more readable
+              target: 'pino-pretty',
+              options: {
+                // allows logs to span multiple lines for better readability
+                singleLine: false,
+              },
+            },
+            serializers: {
+              // only include id, method, and url from requests
+              req: (req) => ({
+                id: req.id,
+                method: req.method,
+                url: req.url,
+              }),
+            },
+          },
+        })
+      : LoggerModule.forRoot(),
   ],
   controllers: [],
   providers: [CourseService, OpenAiService, YoutubeService, PrismaService],
